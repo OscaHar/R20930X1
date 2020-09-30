88e65de609e9aef790a85d2e4aeb5daa5d44bc6c
Oscar Harrysson <harrysson.oscar@gmail.com>
2020-09-30 18:03:16 +0200

    ADD: error page

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get('*', (req, res) => {
+
+    res.sendFile(path.join(__dirname + '/error.html'));
+})
+
 
 app.listen(PORT, () => {
     console.log(`app listening at http://localhost:${PORT}`)
